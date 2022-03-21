3586c47a69aa7972f8147c3f013a4175f31d368f
derinto <102010334+derinto@users.noreply.github.com>
2022-03-21 16:34:54 +0700

    Update contoh1.php

--- contoh1.php
+++ contoh1.php
@@ -3,8 +3,8 @@ class Contoh1 extends CI_Controller{
     public function index()
     {
         echo"<h1>Hallo</h1>";
-        echo"Nama  Claudyana Gabrillia Evitania
-        Alamat Villa Pabuaran Indah"
+        echo"Nama  Muhammad Derinto Prakoso
+         Alamat Villa Pabuaran Indah"
 
         }
 }
